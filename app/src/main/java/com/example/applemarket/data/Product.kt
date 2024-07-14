3b75248c841a38786e8c492efdad5a36ebc18774
idkos <leegang31@gmail.com>
2024-07-14 22:23:12 +0900

    add: Product 클래스 생성

---
+++ app/src/main/java/com/example/applemarket/data/Product.kt
@@ -0,0 +1,18 @@
+package com.example.androidtask.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Product(
+    val id: Int, // 번호
+    val image: Int, // 이미지 파일명
+    val name: String, // 상품명
+    val description: String, // 상품 소개
+    val seller: String, // 판매자
+    val price: Int, // 가격
+    val address: String, // 주소
+    var likes: Int, // 좋아요
+    val chat: Int, // 채팅
+    var isLiked: Boolean = false // 좋아요 여부
+) : Parcelable
