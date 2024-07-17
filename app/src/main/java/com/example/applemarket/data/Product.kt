e5a84efadb9a518201b2f209177c42204fcac9d6
idkos <leegang31@gmail.com>
2024-07-17 20:30:27 +0900

    refactor: 주석 가독성 향상

--- app/src/main/java/com/example/applemarket/data/Product.kt
+++ app/src/main/java/com/example/applemarket/data/Product.kt
@@ -5,14 +5,14 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Product(
-    val id: Int, // 번호
-    val image: Int, // 이미지 파일명
-    val name: String, // 상품명
-    val description: String, // 상품 소개
-    val seller: String, // 판매자
-    val price: Int, // 가격
-    val address: String, // 주소
-    var likes: Int, // 좋아요
-    val chat: Int, // 채팅
-    var isLiked: Boolean = false // 좋아요 여부
+    val id: Int,                // 번호
+    val image: Int,             // 이미지 파일명
+    val name: String,           // 상품명
+    val description: String,    // 상품 소개
+    val seller: String,         // 판매자
+    val price: Int,             // 가격
+    val address: String,        // 주소
+    var likes: Int,             // 좋아요
+    val chat: Int,              // 채팅
+    var isLiked: Boolean = false// 좋아요 여부
 ) : Parcelable
